ee2796d300f38b6575f55c0578562a21acda6300
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-26 02:59:46 -0700

    Added Protocols Library
    Converted library to C#

---
+++ Protocols/Properties/AssemblyInfo.cs
@@ -0,0 +1,47 @@
+// VBConversions Note: VB project level imports
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.Data;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Linq;
+// End of VB project level imports
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly:AssemblyTitle("Protocols Library")]
+[assembly:AssemblyDescription("")]
+[assembly:AssemblyCompany("Versidyne LLC")]
+[assembly:AssemblyProduct("Protocols Library")]
+[assembly:AssemblyCopyright("Copyright © Versidyne LLC 2010-2013")]
+[assembly:AssemblyTrademark("")]
+
+[assembly:ComVisible(false)]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly:Guid("770e9bbc-52bb-4657-a1b8-ae3fa7b8022d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")>
+
+[assembly:AssemblyVersion("0.1.0.0")]
+[assembly:AssemblyFileVersion("0.1.0.0")]
+
---
+++ Protocols/XML.cs
@@ -0,0 +1,112 @@
+// VBConversions Note: VB project level imports
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.Data;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Linq;
+// End of VB project level imports
+
+
+namespace Versidyne.Protocols
+{
+	public class XML
+	{
+		
+		private string Key = "<|(cell)|>";
+		private string SepKey = "<|(row)|>";
+		private string Version = "0.1.0.0";
+		
+		public string Encode(string ToString, string FromString, string Type, string Data)
+		{
+			
+			string EncodedData = Key + Version + Key + ToString + Key + FromString + Key + Type + Key + Data + Key;
+			
+			return EncodedData;
+			
+		}
+		
+		public string[] Decode(string Code)
+		{
+			
+			string[] DecodedData = null;
+			string[] Keys = new string[] {Key};
+			
+			DecodedData = Code.Split(Keys.ToString().ToCharArray(), StringSplitOptions.None);
+			
+			return DecodedData;
+			
+		}
+		
+		public Array MDConvert(string Data)
+		{
+			
+			//basic variables
+			Array Basic = default(Array);
+			string Row = default(string);
+			
+			//complex variables
+			ArrayList Dimensions = (ArrayList) null;
+			Array Dimension = (Array) null;
+			
+			//intial split to single dimensional array
+			Basic = Data.Split(SepKey.ToCharArray()[0]);
+			
+			//access each row
+			foreach (string tempLoopVar_Row in Basic)
+			{
+				Row = tempLoopVar_Row;
+				
+				//create array out of row
+				Dimension = Row.Split(Key.ToCharArray()[0]);
+				
+				//add to main array
+				Dimensions.Add(Dimension);
+				
+			}
+			
+			return Dimensions.ToArray();
+			
+		}
+		
+		public string MDConvert(Array Dimensions)
+		{
+			
+			//basic variables
+			string Basic = null;
+			
+			//complex variables
+			Array Dimension = default(Array);
+			string Cell = default(string);
+			
+			//access each dimension
+			foreach (Array tempLoopVar_Dimension in Dimensions)
+			{
+				Dimension = tempLoopVar_Dimension;
+				
+				string Row = null;
+				
+				//access each cell
+				foreach (string tempLoopVar_Cell in Dimension)
+				{
+					Cell = tempLoopVar_Cell;
+					
+					//create string out of dimension
+					Row += Key + Cell;
+					
+				}
+				
+				//add to main string
+				Basic += Row;
+				
+			}
+			
+			return Basic;
+			
+		}
+		
+	}
+	
+}
